2331df632b06e8fc23516f69d3344d199460ec64
shreyas710 <62650542+shreyas710@users.noreply.github.com>
2020-05-20 14:54:31 +0530

    Update loan.php

--- loan.php
+++ loan.php
@@ -1,3 +1,13 @@
-<?php
-    echo "hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loan Details</title>
+</head>
+<body>
+    <script>
+        alert("Login successful");
+    </script>
+</body>
+</html>
