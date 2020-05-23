142b10844613ba4b079936e1046d3f40c3e078ee
shreyas710 <62650542+shreyas710@users.noreply.github.com>
2020-05-23 14:29:53 +0530

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "projectdatabase";
-
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
